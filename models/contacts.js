485ca54245e25629ea2a39b2ca55947bd92292f2
ZVA-23 <zva-23@ukr.net>
2022-12-26 17:44:07 +0200

    add owner in contactSchema

--- models/contacts.js
+++ models/contacts.js
@@ -31,6 +31,10 @@ const contactSchema = new Schema(
       type: Boolean,
       default: false,
     },
+    owner: {
+      type: SchemaTypes.ObjectId,
+      ref: "user",
+    },
   },
   { versionKey: false, timestamps: true }
 );
