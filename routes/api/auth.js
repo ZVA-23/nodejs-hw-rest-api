19a435272ff2df57cac0276c5fcca742fc87669c
ZVA-23 <zva-23@ukr.net>
2022-12-26 07:16:58 +0200

    created User model

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const { validateBody } = require("../../middlewares");
 
+const { schemas } = require("../../models/user");
+
 const router = express.Router();
 
 module.exports = router;
