93d2213d7bc08bf0c770329ab562b5dc430e96f9
ZVA-23 <zva-23@ukr.net>
2022-12-28 19:23:45 +0200

    step3 - create uodateAvatar

--- models/user.js
+++ models/user.js
@@ -23,7 +23,9 @@ const userSchema = new Schema(
       enum: ["starter", "pro", "business"],
       default: "starter",
     },
-    token: String,
+    token: {
+      type: String,
+    },
     avatarURL: {
       type: String,
       required: true,
